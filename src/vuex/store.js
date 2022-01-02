6f3c6744d08d13d1da64234504dd086d169a0dc2
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-02 19:57:37 +0200

    feat: add include vuex in our app

---
+++ src/vuex/store.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
+
+export default store;
